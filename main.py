237dd0b63bcb611297c2e761f85ee679639dc6a6
asadbek <asadbekmirtazayev0@gmail.com>
2022-12-23 10:30:22 +0500

    created requirements.txt

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from routers import auth_router, flowers_router
 
 app = FastAPI()
 
-origins = {
-    "http://localhost",
-    "http://localhost:3000",
-}
+# origins = {
+#     "http://localhost",
+#     "http://localhost:3000",
+# }
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.include_router(flowers_router.router)
 app.include_router(auth_router.router)
 Base.metadata.create_all(bind=engine)
